81b520d84d2d7ef84a729eae49b130f361ff425c
Khalil Estell <khalil.estell@sjsu.edu>
2025-05-23 18:23:45 -0700

    :fire: Remove -ffat-lto (#34)

--- cmake/build.cmake
+++ cmake/build.cmake
@@ -66,7 +66,6 @@ function(libhal_make_library)
     -ffunction-sections
     -fdata-sections
     -flto
-    -ffat-lto-objects
     $<$<COMPILE_LANGUAGE:CXX>:-fexceptions -fno-rtti>)
   target_link_libraries(${LIBRARY_ARGS_LIBRARY_NAME} PUBLIC
     ${LIBRARY_ARGS_LINK_LIBRARIES})
@@ -240,7 +239,6 @@ function(libhal_build_demos)
     -ffunction-sections
     -fdata-sections
     -flto
-    -ffat-lto-objects
     ${DEMO_ARGS_COMPILE_FLAGS}
     $<$<COMPILE_LANGUAGE:CXX>:-fexceptions -fno-rtti>
   )
@@ -269,7 +267,6 @@ function(libhal_build_demos)
       -ffunction-sections
       -fdata-sections
       -flto
-      -ffat-lto-objects
       ${DEMO_ARGS_COMPILE_FLAGS}
       $<$<COMPILE_LANGUAGE:CXX>:-fexceptions -fno-rtti>
     )
